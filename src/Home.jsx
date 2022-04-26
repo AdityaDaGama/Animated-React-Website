d5a87aef66de0d5baf19308b6eec630927dff6ed
unknown <beingmyselfad@gmail.com>
2022-04-26 15:42:53 +0530

    initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Home = () => {
+return(
+
+  <>
+ <section id = "header" className="d-flex  align-items-center">
+   <div className="container-fluid-nav_bg">
+     <div className="row">
+       <div className="col-10 mx-auto">
+         <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+           <h1 >Filmmaking with <strong className="brand-name">Aditya</strong></h1>
+           <h2>Lorem ipsum dolor sit amet.</h2>
+           <div className="mt-3"><a href=" " className="btn btn-primary">Get stareted</a></div>
+         </div>
+       </div>
+     </div>
+   </div>
+ </section>
+
+
+  </>
+);
+
+
+};
+export default Home;
